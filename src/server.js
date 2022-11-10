d9c7ae96f64bb222c2ed07d30710b66fec222e54
Khalil Mohammed Shams <shamskhalil@gmail.com>
2022-11-10 18:16:09 +0100

    edited server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app
     .use(express.json())
     .use(express.urlencoded({ extended: false }))
     .get('/', (req, res) => {
-        res.status(200).json({ ok: true, message: 'Welcome to my Node App!' })
+        res.status(200).json({ ok: true, message: 'Welcome to my automated pipeline nodejs App!!!!!' })
     })
     .listen(3000, () => {
         console.log('Server listening on port 3000!');
